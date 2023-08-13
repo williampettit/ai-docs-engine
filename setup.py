95bf348e65fc29f777a040a339d54543bf134d45
williampettit <14142910+williampettit@users.noreply.github.com>
2023-08-13 12:16:03 -0400

    create setup.py for proj

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as file:
+  long_description = file.read()
+
+setup(
+  # basic info
+  name="ai-docs-engine",
+  version="0.1.0",
+  author="William Pettit",
+  license="MIT",
+  description="`Experimental AI docstring tool",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/williampettit/ai-docs-engine",
+  project_urls={
+    "Source Code": "https://github.com/williampettit/ai-docs-engine",
+  },
+  packages=find_packages(),
+  package_dir={
+    "ai_docs_engine": "ai_docs_engine",
+  },
+
+  # requirements
+  python_requires=">=3.6",
+  install_requires=[], # TODO: move contents of requirements.txt here
+
+  # testing
+  test_suite="tests",
+  tests_require=[
+    "pytest",
+  ],
+)
